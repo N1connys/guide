e15e0047c11143d26686e4c3d8b618e182897264
“菜鸟啦啦啦” <2797228224@qq.com>
2023-01-08 13:55:15 +0800

    first commit

---
+++ .history/docusaurus.config_20230108123639.js
@@ -0,0 +1,114 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+// With JSDoc @type annotations, IDEs can provide config autocompletion
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+(module.exports = {
+  title: 'N1connys的个人博客',
+  tagline: '心血来潮，用docusurus搭建的博客，用于记录生活和学习记录',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'N1connys', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
+
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        // docs: {
+        //   sidebarPath: require.resolve('./sidebars.js'),
+        //   // Please change this to your repo.
+        //   editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        // },
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/N1connys/hexoblog',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      navbar: {
+        title: 'N1connys site',
+        logo: {
+          alt: 'My Site Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {
+            type: 'doc',
+            docId: 'intro',
+            position: 'left',
+            label: '搭建教程',
+          },
+          {to: '/blog', label: '博客', position: 'left'},
+          {
+            href: 'https://space.bilibili.com/179309941?spm_id_from=333.1007.0.0',
+            label: 'bilibili',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: '搭建教程',
+            items: [
+              {
+                label: '搭建教程',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'bilibili',
+                href: 'https://space.bilibili.com/179309941?spm_id_from=333.1007.0.0',
+              },
+              {
+                label: 'Github',
+                href: 'https://github.com/N1connys',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: '博客',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
+});
---
+++ .history/src/components/HomepageFeatures_20230108134812.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './HomepageFeatures.module.css';
+
+const FeatureList = [
+  {
+    title: 'hello hello',
+    Svg: require('../../static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+       这个是用docusaurus做的一个个人博客我觉得docusaurus的主题我很喜欢，加上他本来就是一个偏文档类型
+       所以我用这个选择做自己的个人博客。
+      </>
+    ),
+  },
+  {
+    title: '关于我自己',
+    Svg: require('../../static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>我的名字大家可以叫我N1conns(尼寇尼斯)，我在B站上的名字叫做呃呃Kennyz，目前是大二，寒假在学习一些后端知识和算法
+      这几天刚刚才学会几个基础排序和KMP 回溯 递归 等待这些算法，
+      不得不说算法还是很精巧的，大家可以在<code>bilibili</code> B站来联系我，一起交流.
+      </>
+    ),
+  },
+  {
+    title: '我的目标',
+    Svg: require('../../static/img/undraw_docusaurus_react.svg').default,
+    description: (
+      <>
+      打算寒假leetcode刷到150题，把计组，算法和数据库给摸清楚，
+      然后学习java一些写几个项目，贯彻前端和后端的，目前我写的项目都是前后端分离的那种      </>
+    ),
+  },
+];
+
+function Feature({Svg, title, description}) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} alt={title} />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures() {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
